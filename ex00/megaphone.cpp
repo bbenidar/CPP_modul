125913d64df538c3b3b1f0ca7bc50356849b02bc
kali <kali@root.root>
2023-09-29 18:54:51 -0400

    solving ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    if (ac == 1)
+        std :: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE";
+    else
+    {
+        for(int i = 1 ;av[i]; i++)
+            for(int j = 0; av[i][j]; j++)
+                std :: cout << (char)toupper(av[i][j]) ;
+    }
+    std :: cout << "\n";
+}
